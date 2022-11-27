e9f335d6039b9b4942f63b542646041e2f222976
rodrigo samayoa <toti1998sama@gmail.com>
2022-11-27 18:14:00 +0100

    WIP: Mostrar textura

--- ModuleTexture.cpp
+++ ModuleTexture.cpp
@@ -14,20 +14,9 @@ ModuleTexture::~ModuleTexture()
 // Called before render is available
 bool ModuleTexture::Init()
 {
-	float buffer_data[] = {
-		-1.0f, -1.0f, 0.0f, // v0 pos
-		 1.0f, -1.0f, 0.0f, // v1 pos
-		 0.0f, 1.0f, 0.0f, // v2 pos
-
-		 0.0f, 0.0f, // v0 texcoord
-		 1.0f, 0.0f, // v1 texcoord
-		 0.5f, 1.0f // v2 texcoord
-	};
-
-	/*
 	HRESULT hr;
-	DirectX::ScratchImage *image = nullptr;
-	hr = DirectX::LoadFromWICFile(L"./Assests/baboon.png", DirectX::WIC_FLAGS_NONE, nullptr, *image);
+	DirectX::ScratchImage* image = new DirectX::ScratchImage();
+	hr = DirectX::LoadFromWICFile(L"./../Assets/baboon.png", DirectX::WIC_FLAGS_NONE, nullptr, *image);
 
 	if (FAILED(hr))
 	{
@@ -36,9 +25,34 @@ bool ModuleTexture::Init()
 		return false;
 	}
 
+	HRESULT flipResult;
+	DirectX::ScratchImage* resultImage = new DirectX::ScratchImage;
+
+	flipResult = DirectX::FlipRotate(image->GetImages(), image->GetImageCount(), image->GetMetadata(), DirectX::TEX_FR_FLIP_VERTICAL, *resultImage);
+	
+	if (FAILED(flipResult))
+	{
+		ENGINE_LOG("Error al flipear la imagen");
+
+		return false;
+	}
+
+	/*
+	unsigned int texture;
+	glGenTextures(1, &texture);
+	glBindTexture(GL_TEXTURE_2D, texture);
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, image->GetMetadata().width, image->GetMetadata().height, 0, GL_BGR, GL_UNSIGNED_BYTE, resultImage);
+	glGenerateMipmap(GL_TEXTURE_2D);
+	glActiveTexture(texture);
+
 	glEnableVertexAttribArray(1);
-	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 0, (void*)sizeof(float) * 3 * 3);
-	glActiveTexture(GL_TEXTURE0);
+	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 0, (void*)sizeof(float)*3*3);
 	glActiveTexture(GL_TEXTURE5);
 	glBindTexture(GL_TEXTURE_2D, texture_object);*/
 
